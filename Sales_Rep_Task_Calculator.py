d706444053467937425f2cd1b84f03b207e45ee6
Hakob <hkeshis911@student.glendale.edu>
2023-01-19 02:50:53 -0800

    Update Sales_Rep_Task_Calculator.py

--- Sales_Rep_Task_Calculator.py
+++ Sales_Rep_Task_Calculator.py
@@ -14,7 +14,7 @@ class Sales_Tasks_Data():
         Total = Rejection + No_Respopnse + Hung_Up_Phone + Potential_Client
 
         # First Part Of Progress Calculation
-        Total_Clients_From_Weekly_List = int(input("What is the total number of Prospective Clients fro Weekly List? "))
+        Total_Clients_From_Weekly_List = int(input("What is the total number of Prospective Clients from Weekly List? "))
         Remaining_Calls = Total_Clients_From_Weekly_List - Total
 
         # Final Message Result To Give Progress
@@ -34,7 +34,7 @@ class Sales_Tasks_Data():
         Total = Close_Account + No_Respopnse + Keep_Account + Potential_Client + Business_Closed
 
         # First Part Of Progress Calculation
-        Total_Clients_From_Weekly_List = int(input("What is the total number of Inactive Clients from Weekly List? "))
+        Total_Clients_From_Weekly_List = int(input("What is the total number of Inactive Clients from Weekly List?"))
         Remaining_Calls = Total_Clients_From_Weekly_List - Total
 
         # Final Message Result To Give Progress
@@ -44,12 +44,18 @@ class Sales_Tasks_Data():
 
     def Total_Sales_Made():
         Total_Leads_Closed = int(input("How many clients have you made a sale for today? "))
-        List = []
+        List = list()
 
         for i in range(0, Total_Leads_Closed):
-            Sale_Made = int(input("Record your sale made: "))
-            List.append(Sale_Made)
-            i += 1
+            try: 
+                Sale_Made = float(input("Record your sale made: "))
+                List.append(Sale_Made)
+                i += 1
+            except:
+                print("Sorry only enter a value without any other characters like $")
+                Sale_Made = float(input("Record your sale made: "))
+                List.append(Sale_Made)
+                i += 1
 
         print(f"Here is a wide look of all the Sales You Made {List}")
 
@@ -58,9 +64,9 @@ class Sales_Tasks_Data():
                 Total = sum(List)
                 index += 1
 
-        print(f"The Total Amount of Sales You Made Today is {Total}")
+        print(f"The Total Amount of Sales You Made Today is ${Total}")
 
 
-Sales_Tasks_Data.Total_Prospective_Clients_Outreached()
-Sales_Tasks_Data.Total_Inactive_Clients_Outreached()
-Sales_Tasks_Data.Total_Sales_Made()
+# Sales_Tasks_Data.Total_Prospective_Clients_Outreached()
+# Sales_Tasks_Data.Total_Inactive_Clients_Outreached()
+# Sales_Tasks_Data.Total_Sales_Made()
