5473523a30a7b3da85ebc7e24e3b6d417b35e005
Hakob <hkeshis911@student.glendale.edu>
2023-01-21 04:53:35 -0800

    Update Sales_Rep_Task_Calculator.py

--- Sales_Rep_Task_Calculator.py
+++ Sales_Rep_Task_Calculator.py
@@ -1,17 +1,39 @@
 import datetime
 from datetime import date
 
+Reused_Exception_Quote = 'Invalid Number. Please Enter A Valid Amount Of Clients'
+
 
 class Sales_Tasks_Data():
     def Total_Prospective_Clients_Outreached():
         # User Input For Some Common Outcomes After Outreaching A Prospective Client
-        Rejection = int(input("Total Clients That Gave A Rejection From The Sales Pitch: "))
-        No_Respopnse = int(input("Total Clients That Did Not Respond To Outreach: "))
-        Hung_Up_Phone = int(input("Total Clients That Hung Up The Phone Before Sale: "))
-        Potential_Client = int(input("Total Clients That Are Potential Clients: "))
+        try:
+            Rejection = int(input("Total Clients That Gave A Rejection From The Sales Pitch: "))
+
+        except:
+            Rejection = int(input(f"{Reused_Exception_Quote} That Give A Rejection: "))
+
+        try:
+            No_Resopnse = int(input("Total Clients That Did Not Respond To Outreach: "))
+
+        except:
+            No_Resopnse = int(input(f"{Reused_Exception_Quote} That Did Not Respond"))
+
+        try:
+            Hung_Up_Phone = int(input("Total Clients That Hung Up The Phone Before Sale: "))
+
+        except:
+            Hung_Up_Phone = int(input(f"{Reused_Exception_Quote} That Hung Up The Phone Before Sale: "))
+
+        try:
+            Potential_Client = int(input("Total Clients That Are Potential Clients: "))
+
+        except:
+            Potential_Client = int(input(f"{Reused_Exception_Quote} That Are Potential Clients: "))
+
 
         # Sum Calculation from all Inputed Data
-        Total = Rejection + No_Respopnse + Hung_Up_Phone + Potential_Client
+        Total = Rejection + No_Resopnse + Hung_Up_Phone + Potential_Client
 
         # First Part Of Progress Calculation
         Total_Clients_From_Weekly_List = int(input("What is the total number of Prospective Clients from Weekly List? "))
@@ -24,11 +46,35 @@ class Sales_Tasks_Data():
 
     def Total_Inactive_Clients_Outreached():
         # User Input For Some Common Outcomes After Outreaching An Inactive Client
-        Close_Account = int(input("Total Clients That Want To Close Their Account: "))
-        No_Respopnse = int(input("Total Clients That Did Not Respond To Outreach: "))
-        Keep_Account = int(input("Total Clients That Want To Keep Their Account: "))
-        Potential_Client = int(input("Total Clients That Are Potential Clients: "))
-        Business_Closed = int(input("Total Clients That Have Closed Their Business: "))
+        try:
+            Close_Account = int(input("Total Clients That Want To Close Their Account: "))
+
+        except:
+            Close_Account = int(input(f"{Reused_Exception_Quote} That Want To Close Their Account: "))
+
+        try:
+            No_Respopnse = int(input("Total Clients That Did Not Respond To Outreach: "))
+
+        except:
+            No_Respopnse = int(input(f"{Reused_Exception_Quote} That Did Not Respond To Outreach: "))
+
+        try:
+            Keep_Account = int(input("Total Clients That Want To Keep Their Account: "))
+
+        except:
+            Keep_Account = int(input(f"{Reused_Exception_Quote} That Want To Keep Their Account: "))
+
+        try:
+            Potential_Client = int(input("Total Clients That Are Potential Clients: "))
+
+        except:
+            Potential_Client = int(input(f"{Reused_Exception_Quote} That Are Potential Clients: "))
+
+        try:
+            Business_Closed = int(input("Total Clients That Have Closed Their Business: "))
+
+        except:
+            Business_Closed = int(input(f"{Reused_Exception_Quote} That Have Closed Their Business: "))
 
         # Sum Calculation from all Inputed Data
         Total = Close_Account + No_Respopnse + Keep_Account + Potential_Client + Business_Closed
@@ -47,7 +93,7 @@ class Sales_Tasks_Data():
         List = list()
 
         for i in range(0, Total_Leads_Closed):
-            try: 
+            try:
                 Sale_Made = float(input("Record your sale made: "))
                 List.append(Sale_Made)
                 i += 1
